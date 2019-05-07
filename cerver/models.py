34df416ad7c456e39326731b5fd713c708610ff8
Aditya Bansal <adi.bansal241996@gmail.com>
2019-05-07 18:34:47 +0530

    models.py: Add basic Question model for storing questions for forms.

--- cerver/models.py
+++ cerver/models.py
@@ -6,4 +6,30 @@ class Form(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
 
+class Question(models.Model):
+    qheadline = models.CharField(max_length=150)
+    qdescription = models.CharField(max_length=500)
+
+    TEXT = 1
+    INT = 2
+    BOOL = 3
+
+    QUESTION_TYPES = (
+        (TEXT, 'Input text to answer'),
+        (INT, 'Input an Integer to answer'),
+        (BOOL, 'Select from True/False'),
+    )
+
+    question_type = models.PositiveSmallIntegerField(
+        choices=QUESTION_TYPES,
+        default=TEXT
+    )
+
+    form = models.ForeignKey(
+        Form,
+        related_name='form_questions',
+        on_delete=models.CASCADE
+    )
+
 admin.site.register(Form)
+admin.site.register(Question)
