84dd42c6176e91796c4ad807a95fbffedec617e7
Aditya Bansal <adi.bansal241996@gmail.com>
2019-05-07 18:27:01 +0530

    models.py: Register Form with django-admin for basic data entry UI.

--- cerver/models.py
+++ cerver/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Form(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
+
+admin.site.register(Form)
