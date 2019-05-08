ad61a2b03a3d74754967c37dca104c1510af1fa7
Aditya Bansal <adi.bansal241996@gmail.com>
2019-05-08 14:46:03 +0530

    forms.py: Add a export to sheets endpoint.
    This endpoint is strictly for testing purposes and not really meant to
    endup a userfacing thing as per right now plan. Though I see it might also
    make sense for it to be user facing and obviously there are always tradeoff's.

--- collect/urls.py
+++ collect/urls.py
@@ -20,6 +20,7 @@ from cerver.views.forms import (
     handle_response_backend,
     handle_form_creation,
     handle_form_add_question,
+    handle_export,
 )
 
 urlpatterns = [
@@ -27,4 +28,5 @@ urlpatterns = [
     path('collect/forms', handle_form_creation),
     path('collect/forms/<int:form_id>/add_question', handle_form_add_question),
     path('collect/forms/<int:form_id>/response', handle_response_backend),
+    path('collect/forms/<int:form_id>/export', handle_export),
 ]
